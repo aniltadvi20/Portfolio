d21f1df55abdcb4ac2936ebbefa76ed796468527
Anil Tadvi <185767034+aniltadvi20@users.noreply.github.com>
2026-01-28 16:16:59 +0530

    Create Blog.jsx

---
+++ client/src/pages/Blog.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div
+      style={{
+        color: "white",
+        padding: "120px",
+        fontSize: "32px",
+        backgroundColor: "#0A192F",
+        minHeight: "100vh",
+      }}
+    >
+      BLOG PAGE WORKING ✅
+    </div>
+  );
+};
+
+export default Blog;
